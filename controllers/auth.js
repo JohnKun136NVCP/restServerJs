ab4e0c2dc67fad2f6dd8a52f85fc5cd0be939593
桂馬達也 <73410627+JohnKun136NVCP@users.noreply.github.com>
2025-10-08 14:22:09 -0600

    [ADDED][4.0.0-rc-l][OWNER] Added Google OAuth(old version)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const {response} = require('express');
+const {response, json} = require('express');
 const bcryptjs = require('bcryptjs');
 const User = require('../models/user');
 const { generateJWT } = require('../helpers/generate-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req,res = response) =>{
     const {email, password} =req.body;
@@ -47,7 +48,54 @@ const login = async (req,res = response) =>{
 
 
 }
+const googleSignIn = async (req,res =response) =>{
+    const {id_token} = req.body;
+    
+    try {
+        const {name,email,picture} = await googleVerify(id_token);
+        let user = await User.findone({ email });
+
+
+        if(!user){
+            //Create user
+            const data = {
+                name,
+                email,
+                password: "xD",
+                picture,
+                google: true
+            };
+            user = new User( data );
+            await user.save();
+        }
+
+        //User in DB
+        if(!user.state){
+            return res.status(401).json({
+                msg: 'Contact with the administrator, user blocked'
+            })
+        }
+
+        //Generate JWT
+        const token = await generateJWT(user.id);
+        res.json({
+            user,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            ok:false,
+            msg:"Token wrong!"
+        })
+    }
+
+
+
+}
+
+
 
 module.exports={
-    login
+    login,
+    googleSignIn
 }
